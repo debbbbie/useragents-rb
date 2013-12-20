3726dc499a53a947802cea59bbd41710643dbe3e
debbbbie <debbbbie@163.com>
2013-12-20 14:13:40 +0800

    Update

--- fetch.rb
+++ fetch.rb
@@ -1,8 +1,17 @@
 require 'nokogiri'
 require 'open-uri'
 
-url = 'http://www.useragentstring.com/pages/Chrome/'
-doc = Nokogiri::HTML(open(url))
-doc.css('#liste ul li a').each do |link|
-  puts link.content
+urls = [
+    'http://www.useragentstring.com/pages/Chrome/',
+    'http://www.useragentstring.com/pages/Firefox/',
+    'http://www.useragentstring.com/pages/Internet%20Explorer/',
+    'http://www.useragentstring.com/pages/Opera/',
+    'http://www.useragentstring.com/pages/Safari/'
+]
+
+agents = urls.inject([]) do |sum, url|
+  doc  = Nokogiri::HTML(open(url))
+  sum + doc.css('#liste ul li a').map{|link| link.content.strip }
 end
+
+File.write(File.expand_path('../lib/useragents.txt', __FILE__), agents.join("\n"))
